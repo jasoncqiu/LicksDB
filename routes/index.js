372a763592cfd8b309dee52c3fab6942b0e73d7c
jasoncqiu <jasoncqiu@gmail.com>
2015-01-14 01:14:23 -0800

    some bootstrap to index.jade

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'LickDB', types: ['major', 'minor', 'dominant'] });
+  res.render('index', { title: 'LicksDB', types: ['major', 'minor', 'dominant'] });
 });
 
 /* hello world page. */
