443dfc86f6243912e01b6dc9841ad93621177fde
Jack Kwan <jack.ks.kwan@gmail.com>
2015-01-14 20:15:44 -0500

    added editor page

--- routes/index.js
+++ routes/index.js
@@ -74,4 +74,53 @@ router.post('/select_type', function(req, res) {
     });
 });
 
+// new lick page
+router.get('/newlick', function(req, res) {
+    res.render('newlick');
+});
+
+//POST to new lick submit
+router.post('/licksubmit', function(req, res) {
+
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var type = req.body.type;
+    var key = req.body.key;
+    var meter = req.body.meter;
+    var licktext = req.body.licktext;
+    // console.log(key);
+    // console.log(meter);
+    // console.log(licktext);
+    console.log("before:\n");
+    console.log(licktext);
+    console.log("after:\n");
+    licktext = licktext.split("\n").join("\\n"); //replace all /n with //n's
+    licktext = licktext.split("\r").join("\\r"); //replace all /n with //n's
+    //licktext = licktext.replace("\n", "\\n");
+    console.log(licktext);
+    // Set our collection
+    var collection = db.get('licks');
+
+    // Submit to the DB
+    collection.insert({
+        "type" : type,
+        "key" : key,
+        "meter" : meter,
+        "lick" : licktext
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            res.location("/");
+            // And forward to success page
+            res.redirect("/");
+        }
+    });
+});
+
 module.exports = router;
