86f0ecccdce09c07ecb9505c24df2cd15095b759
jasoncqiu <jasoncqiu@gmail.com>
2015-01-15 04:02:43 -0800

    update keys and meters.

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'LicksDB', types: ['major', 'minor', 'dominant'] });
-});
-
-/* hello world page. */
-router.get('/helloworld', function(req, res) {
-  res.render('helloworld', { title: 'Hello, World!' });
-});
-
-/* GET Userlist page. */
-router.get('/userlist', function(req, res) {
-    var db = req.db;
-    var collection = db.get('usercollection');
-    collection.find({},{},function(e,docs){
-        res.render('userlist', {
-            "userlist" : docs
-        });
-    });
+  res.render('index', { title: 'LicksDB',
+    types: ['major', 'minor', 'dominant'],
+    keys: ['C', 'C#/Db', 'D', 'D#/Eb', 'E', 'F', 'F#/Gb', 'G', 'G#/Ab', 'A', 'A#/Bb', 'B'],
+    meters: ['4/4', '3/4', '3/8']});
 });
 
 /* GET New User page. */
@@ -27,49 +14,19 @@ router.get('/newuser', function(req, res) {
     res.render('newuser', { title: 'Add New User' });
 });
 
-
-/* POST to Add User Service */
-router.post('/adduser', function(req, res) {
-
-    // Set our internal DB variable
-    var db = req.db;
-
-    // Get our form values. These rely on the "name" attributes
-    var userName = req.body.username;
-    var userEmail = req.body.useremail;
-
-    // Set our collection
-    var collection = db.get('usercollection');
-
-    // Submit to the DB
-    collection.insert({
-        "username" : userName,
-        "email" : userEmail
-    }, function (err, doc) {
-        if (err) {
-            // If it failed, return error
-            res.send("There was a problem adding the information to the database.");
-        }
-        else {
-            // If it worked, set the header so the address bar doesn't still say /adduser
-            res.location("userlist");
-            // And forward to success page
-            res.redirect("userlist");
-        }
-    });
-});
-
-router.post('/select_type', function(req, res) {
+router.post('/select_parameters', function(req, res) {
     var type = req.body.selected_type;
-    // var key = req.body.selected_key;
-    // var meter = req.body.selected_meter;
+    var key = req.body.selected_key;
+    var meter = req.body.selected_meter;
     var db = req.db;
     var licks = db.get('licks');
-    licks.find({"type": type},{},function(e,docs){
+    licks.find({'type': type, 'key': key, 'meter': meter},{},function(e,docs){
         res.render('results', {
             "results" : docs,
             title: 'Results',
-            type: type
+            type: type,
+            key: key,
+            meter: meter
         });
     });
 });
